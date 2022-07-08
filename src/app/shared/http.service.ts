65751f758418736a17be545a4e40689769b282aa
Dim@ <dzmitry.kaporski@gmail.com>
2022-07-08 20:39:44 +0300

    refactor: update link for deploy

--- src/app/shared/http.service.ts
+++ src/app/shared/http.service.ts
@@ -5,7 +5,7 @@ import { catchError, Observable, of } from 'rxjs';
 
 import { Customer } from './customer';
 
-const url = 'https://angular-crud-dk-default-rtdb.asia-southeast1.firebasedatabase.app/'
+const url = 'https://angular-crud-data-default-rtdb.firebaseio.com/'
 const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
 
 @Injectable({
