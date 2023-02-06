9e4190c0ac2fa1d2eb21385c7956f320f68c39e9
dima <dzmitry.kaporski@gmail.com>
2023-02-06 08:43:52 +0300

    refactor: modified src/app/shared/http.service.ts

--- angular-CRUD/src/app/shared/http.service.ts
+++ angular-CRUD/src/app/shared/http.service.ts
@@ -5,7 +5,7 @@ import { catchError, Observable, of } from 'rxjs';
 
 import { Customer } from './customer';
 
-const url = 'https://angular-crud-realtime-da-f01ab-default-rtdb.firebaseio.com/'
+const url = 'https://angular-crud-database-209e3-default-rtdb.firebaseio.com/'
 const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
 
 @Injectable({
