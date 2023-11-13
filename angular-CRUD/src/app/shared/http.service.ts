56ade5a474de7bfe0194c885a1648a842a5075ea
Dim@ S <dzmitry.kaporski@gmail.com>
2023-11-13 15:30:39 +0300

    refactor: update link for realtime database

--- angular-CRUD/src/app/shared/http.service.ts
+++ angular-CRUD/src/app/shared/http.service.ts
@@ -5,7 +5,7 @@ import { catchError, Observable, of } from 'rxjs';
 
 import { Customer } from './customer';
 
-const url = 'https://angular-crud-realtime-da-e4750-default-rtdb.asia-southeast1.firebasedatabase.app/'
+const url = 'https://angular-crud-realtime-dk-default-rtdb.firebaseio.com/'
 const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
 
 @Injectable({
