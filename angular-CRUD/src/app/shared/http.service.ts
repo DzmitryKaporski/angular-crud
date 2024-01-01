0b6f199f424ab8bcb090760e57afbfe87f8d4dde
Dim@ S <dzmitry.kaporski@gmail.com>
2024-01-01 21:40:56 +0300

    refactor: update link for firebase

--- angular-CRUD/src/app/shared/http.service.ts
+++ angular-CRUD/src/app/shared/http.service.ts
@@ -5,7 +5,7 @@ import { catchError, Observable, of } from 'rxjs';
 
 import { Customer } from './customer';
 
-const url = 'https://angular-crud-realtime-da-d224e-default-rtdb.europe-west1.firebasedatabase.app/'
+const url = 'https://angular-crud-realtime-da-1a941-default-rtdb.firebaseio.com/'
 const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) }
 
 @Injectable({
